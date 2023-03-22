feff0755c5041324713c2da9e3489a22c59f285d
AadiNir <aadiniranjan27@gmail.com>
2023-03-22 10:50:45 +0530

    New commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react'
 
 function App() {
+  const [data,setdata]=useState([{}])
+  useEffect(()=>{
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data=>{
+        setdata(data)
+      }
+    )
+  },[])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    {(typeof data.members==='undefined')?(<p>Loading</p>):
+      data.members.map((member,i)=>(
+      <h1 key={i}>{member}</h1>
+
+    ))}
+    
+    
     </div>
   );
 }
